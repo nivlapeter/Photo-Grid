e4c02e9ba7666d354803ea2b6854fb220aecd33f
nivlapeter <nivlapeter.0549@gmail.com>
2020-10-13 10:04:32 +0300

    register models to admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
+from .models import Uploader,Image,Location
 
 # Register your models here.
-# class ImageAdmin(admin.ModelAdmin):
-#     filter_horizontal = ('Location',)
-
-# admin.site.register(Uploader)
-# admin.site.register(Image,ImageAdmin)
-# admin.site.register(Location)
+    
+admin.site.register(Uploader)
+admin.site.register(Image)
+admin.site.register(Location)
