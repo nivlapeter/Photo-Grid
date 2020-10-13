eb289e767000275636755e306fde93d4ddbfe8fd
nivlapeter <nivlapeter.0549@gmail.com>
2020-10-13 09:44:01 +0300

    Adding DB

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD':os.environ.get('DB_PASSWORD'),
