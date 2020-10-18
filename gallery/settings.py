ac1dffece41d52b20f3b45e7816f4a23078b7f4b
nivlapeter <nivlapeter.0549@gmail.com>
2020-10-18 09:26:54 +0300

    added django-heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import cloudinary
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,5 @@ STATICFILES_DIRS = [
 #https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
